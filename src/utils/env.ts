d1c234ad9c7bf729e6de0481be9ba3bb18b12c0c
EdissonBJ <edissonbj@outlook.com>
2026-06-10 10:28:44 -0500

    feat: estructura POM login con Playwright

---
+++ src/utils/env.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import path from 'path'
+
+//Asegurar carga desde la raiz
+dotenv.config ({path: path.resolve(__dirname, '../../.env')});
+
+export const getEnv = () =>({
+    baseUrl: process.env.BASE_URL!,
+    username: process.env.USERNME!,
+    password: process.env.password!,
+
+});
---
+++ tests/login.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../src/pages/LoginPage.js';
+import { getEnv } from '../src/utils/env';
+
+
+
+test('Login exitoso con usuario estándar', async ({page})=>{
+    const loginPage=new LoginPage(page);
+    const {username, password}= getEnv();
+
+    await loginPage.goto();
+    await loginPage.login(username, password);
+
+    await expect(page).toHaveURL(/inventory.html/);
+});
+
+
+test('Login falla con credenciales inválidas', async ({page}) =>{
+
+    const loginPage = new LoginPage(page);
+
+    await loginPage.goto();
+    await loginPage.login('usuario invalido','clave mal');
+
+    const error =await loginPage.getErrorMessage();
+    expect(error).toContain('Username and password no match');
+
+});
